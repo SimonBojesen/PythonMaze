da2e2331cab6ce695139600984327e415e9fddbf
Alek2750 <alek2750@gmail.com>
2019-12-16 14:20:40 +0100

    Plot needs some work
    problem with min, max and median because x & y needs to be same size

--- plotSteps.py
+++ plotSteps.py
@@ -1,10 +1,43 @@
 import matplotlib.pyplot as plt
-#import maze_generator
-plt.figure()
-x_values = range(1, 100)
-y_values = [x ** 2 for x in x_values]
-print('Points to be plotted {}'.format(list(zip(x_values, y_values)))) 
-# The purpose of zip() is to map the similar index of multiple containers
-# so that they can be used just using as single entity.
-plt.scatter(x_values, y_values, s=100)
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('numberOfSteps.csv')
+steps = np.array(data['Steps(nodes)']) # reads data from a csv file
+#mazesize = np.array(data['MazeSize'])
+print(steps)
+
+steps5 = [23,26,20,21,30]
+steps10 = [23,26,20,21,30]
+steps15 = [23,26,20,21,30]
+steps20 = [23,26,20,21,30]
+steps25 = [23,26,20,21,30]
+
+
+plt.style.use("fivethirtyeight") # plot styling
+
+x_mazesize = [5,10,15,20,25]
+y_steps = steps
+
+#min = np.min(steps5,steps10,steps15,steps20,steps25)
+#max = np.max(steps5,steps10,steps15,steps20,steps25)
+#median = np.median(steps5,steps10,steps15,steps20,steps25)
+
+plt.plot(x_mazesize, steps, marker="o", label="Steps") # plots the data
+#plt.plot(x_mazesize, min, marker="o", label="Steps") # need to find out how to plot min steps
+#plt.plot(x_mazesize, max, marker="o", label="Steps") # need to find out how to plot max steps
+#plt.plot(x_mazesize, median, marker="o", label="Steps") # need to find out how to plot median steps
+# Can not plot becuase x & y needs to be same size
+
+plt.title("Plot showing max, mini, and average iterations for solving the maze.")
+plt.xlabel("mazesize(x,y)")
+plt.ylabel("Steps(iterations)")
+
+plt.legend()
+#plt.grid(True)
+
+plt.savefig('Plots/plot.png')
+
 plt.show()
+
+#print(min, max, median)
