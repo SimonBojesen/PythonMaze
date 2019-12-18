7dc2c6de16c41ee966e890642419e59c59ca49be
hassuni <hasss95@hotmail.com>
2019-12-18 21:26:31 +0100

    ValueError*

--- GUI2.py
+++ GUI2.py
@@ -10,11 +10,11 @@ mazes = []
 c = MVC.Controller(MVC.Model(), MVC.View())
 
 
-class MazeSizeTooLow(Exception):
+class MazeSizeTooLow(ValueError):
     pass
 
 
-class MazeSizeTooHigh(Exception):
+class MazeSizeTooHigh(ValueError):
     pass
 
 
@@ -53,13 +53,14 @@ def maze_gen():
     f = open(filename)
     maze_text_widget.delete('1.0', END)
     maze_text_widget.insert(END, f.read())
-    #maze_label.config(text=f.read())
+    # maze_label.config(text=f.read())
     load_mazes()
+
 
 def solve_maze():
     global mazes
     maze_index = 0
-    tuple_index = listbox_mazes.curselection() 
+    tuple_index = listbox_mazes.curselection()
     for value in tuple_index:
         maze_index = value
     filename = mazes[maze_index]
@@ -75,6 +76,7 @@ def load_mazes():
         listbox_mazes.insert(END, maze)
     listbox_mazes.place(relwidth=1, relheight=1)
 
+
 def get_maze():
     global mazes
     path = 'SavedMazes/'
@@ -87,7 +89,8 @@ def get_maze():
     f = open(path + filename)
     maze_text_widget.delete('1.0', END)
     maze_text_widget.insert(END, f.read())
-    
+
+
 def update_observer_text(text):
     observer_text_widget.delete('1.0', END)
     observer_text_widget.insert(END, text)
@@ -122,7 +125,8 @@ pub.register(sub)
 
 root = Tk()
 root.title('MazesGUI')
-canvas = Canvas(root, width=window_width, height=windows_height, relief='raised')
+canvas = Canvas(root, width=window_width,
+                height=windows_height, relief='raised')
 canvas.pack()
 
 # main frame for showing maze + buttons
@@ -136,16 +140,19 @@ maze_frame = Frame(main_frame, bd=1, bg='black')
 maze_frame.place(relwidth=0.9, relheight=0.8, relx=0.05, rely=0.05)
 maze_text_widget = Text(maze_frame, font=('verdana', 7))
 maze_text_widget.place(relwidth=1, relheight=1)
-button_create = Button(main_frame, text='Create maze', command=maze_gen, bg='#5AA04B', fg='black', activebackground='#244E1B', font=('verdana', 12))
+button_create = Button(main_frame, text='Create maze', command=maze_gen,
+                       bg='#5AA04B', fg='black', activebackground='#244E1B', font=('verdana', 12))
 button_create.place(relwidth=0.45, relheight=0.08, relx=0.05, rely=0.85)
-button_solve = Button(main_frame, text='Solve maze', command=solve_maze, bg='#5AA04B', fg='black', activebackground='#244E1B', font=('verdana', 12))
+button_solve = Button(main_frame, text='Solve maze', command=solve_maze,
+                      bg='#5AA04B', fg='black', activebackground='#244E1B', font=('verdana', 12))
 button_solve.place(relwidth=0.45, relheight=0.08, relx=0.5, rely=0.85)
 
 # here we have the left frame where we have tried to use observer pattern
 # we write stuff to the user in here like we would normally do in the console
 left_frame = Frame(root, bg='#4C7676')
 left_frame.place(relwidth=0.25, relheight=1)
-top_label_left = Label(left_frame, text='Observer', bg='#4C7676', font=('verdana', 10))
+top_label_left = Label(left_frame, text='Observer',
+                       bg='#4C7676', font=('verdana', 10))
 top_label_left.place(relx=0.1, rely=0.01)
 observer_frame_padding = Frame(left_frame, bd=1, bg='black')
 observer_frame_padding.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.88)
@@ -154,14 +161,16 @@ observer_text_widget.place(relwidth=1, relheight=1)
 
 right_frame = Frame(root, bg='#4C7676')
 right_frame.place(relwidth=0.25, relheight=1, relx=0.75)
-top_label_right = Label(right_frame, text='Mazes', bg='#4C7676', font=('verdana', 10))
+top_label_right = Label(right_frame, text='Mazes',
+                        bg='#4C7676', font=('verdana', 10))
 top_label_right.place(relx=0.1, rely=0.01)
 mazes_frame_padding = Frame(right_frame, bd=1, bg='black')
 mazes_frame_padding.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.8)
 mazes_mainframe = Frame(mazes_frame_padding)
 mazes_mainframe.place(relwidth=1, relheight=1)
 listbox_mazes = Listbox(mazes_mainframe, selectbackground='black')
-select_maze = Button(right_frame, text='Select maze', command=get_maze, bg='#5AA04B', fg='black', activebackground='#244E1B', font=('verdana', 12))
+select_maze = Button(right_frame, text='Select maze', command=get_maze,
+                     bg='#5AA04B', fg='black', activebackground='#244E1B', font=('verdana', 12))
 select_maze.place(relx=0.1, rely=0.85, relwidth=0.8, relheight=0.08)
 
 load_mazes()
