419abe0809548c23f466c607c2e9d2a82aacf61f
SimonBojesen <simon@schonberg.dk>
2019-12-19 00:35:27 +0100

    integration test

---
+++ integration_test.py
@@ -0,0 +1,18 @@
+import unittest
+import test_backend_functionality
+import test_maze_generator
+import test_Maze_Solver
+#test_exceptions doesnt work unfortunately
+import test_exceptions
+
+if __name__ == '__main__':
+    tests1 = unittest.TestLoader().loadTestsFromModule(test_backend_functionality)
+    tests2 = unittest.TestLoader().loadTestsFromModule(test_maze_generator)
+    tests3 = unittest.TestLoader().loadTestsFromModule(test_Maze_Solver)
+    #tests4 = unittest.TestLoader().loadTestsFromModule(test_exceptions)
+
+    unittest.TextTestRunner(verbosity=2).run(tests1)
+    unittest.TextTestRunner(verbosity=2).run(tests2)
+    unittest.TextTestRunner(verbosity=2).run(tests3)
+    #unittest.TextTestRunner(verbosity=2).run(tests4)
+
--- test_Maze_Solver.py
+++ test_Maze_Solver.py
@@ -4,13 +4,13 @@ import backend_functionality
 class TestMazeSolver(unittest.TestCase):
     def setUp(self):
         self.sizeX = 5
-        self.sizeY = 5
+        self.sizeY = 15
         self.maze = backend_functionality.maze_generate(self.sizeX, self.sizeY)
 
     def test_solver_found_goal(self): #Test if solver finds goal and returns true
         backend_functionality.set_grid(self.maze)
         true_or_false = backend_functionality.search(1, 1)
         self.assertTrue(true_or_false, 'Test failed')
-        
+
 if __name__ == '__main__':
     unittest.main()
