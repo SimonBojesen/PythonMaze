e66feb5a2daf1d831c66c3f51deb4bf86b30023d
hassuni <hasss95@hotmail.com>
2019-12-18 15:35:35 +0100

    +2 unittest

--- backend_functionality.py
+++ backend_functionality.py
@@ -3,7 +3,8 @@ import sys
 import time
 import csv
 import platform
-import os, os.path
+import os
+import os.path
 
 # this is a stepcounter
 # it increments by 1 everytime the search function gets called
@@ -15,6 +16,8 @@ sys.setrecursionlimit(10000)
 
 # Each maze cell contains a tuple of directions of cells to which it is connected
 # Takes a maze and converts it to an array of X's and blanks to represent walls, etc
+
+
 def convert(maze):
     pretty_maze = [["1"]*(2*len(maze[0])+1) for a in range(2*len(maze)+1)]
     for y, row in enumerate(maze):
@@ -26,6 +29,8 @@ def convert(maze):
     return pretty_maze
 
 # Takes a converted maze and pretty prints it
+
+
 def pretty_print(maze):
     for a in maze:
         string = ""
@@ -35,12 +40,16 @@ def pretty_print(maze):
     print
 
 # Returns an empty maze of given size
+
+
 def make_empty_maze(width, height):
     maze = [[[] for b in range(width)] for a in range(height)]
     return maze
 
 # Recursive backtracker.
 # Looks at its neighbors randomly, if unvisitied, visit and recurse
+
+
 def DFS(maze, coords=(0, 0)):
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     shuffle(directions)
@@ -50,9 +59,11 @@ def DFS(maze, coords=(0, 0)):
            (0 <= new_coords[1] < len(maze[0])) and \
            not maze[new_coords[0]][new_coords[1]]:
             maze[coords[0]][coords[1]].append(direction)
-            maze[new_coords[0]][new_coords[1]].append((-direction[0], -direction[1]))
+            maze[new_coords[0]][new_coords[1]].append(
+                (-direction[0], -direction[1]))
             DFS(maze, new_coords)
     return maze
+
 
 def search(x, y):
     if grid[x][y] == '2':
@@ -75,20 +86,22 @@ def search(x, y):
     if ((x < (len(grid)-1) and search(x+1, y))
         or (y > 0 and search(x, y-1))
         or (x > 0 and search(x-1, y))
-        or (y < len(grid)-1 and search(x, y+1))):   
+            or (y < len(grid)-1 and search(x, y+1))):
         return True
     return False
+
 
 def increment():
     global stepcount
     stepcount = stepcount+1
 
+
 def write(maze):
     if platform.system() == 'Windows':
-        newline=''
+        newline = ''
     else:
-        newline=None
-    #for filename in os.listdir('SavedMazes'):
+        newline = None
+    # for filename in os.listdir('SavedMazes'):
     #    number + 1
     files = os.listdir("SavedMazes")
     number = len(files) + 1
@@ -98,35 +111,44 @@ def write(maze):
         output_writer.writerows(maze)
         return filename
 
+
 def count_saved_mazes():
     files = os.listdir("SavedMazes")
     print(len(files))
 
+
 def read():
     with open("SavedMazes/mazes.csv") as f:
-        lis = [line.replace("\n","").split(",") for line in f] # create a list of lists
+        lis = [line.replace("\n", "").split(",")
+               for line in f]  # create a list of lists
         return lis
+
 
 def maze_generate(sizeX, sizeY):
     return convert(DFS(make_empty_maze(sizeX, sizeY)))
 
+
 def should_we_solve(selfview, userinput):
-        if userinput == "yes":
-            return True
-        elif userinput == "no":
-            selfview.endView()
-            return False
-        else:
-            yes_or_no = selfview.invalid_input("Should the program run the solving algorithm?\n")
-            should_we_solve(selfview, yes_or_no)
+    if userinput == "yes":
+        return True
+    elif userinput == "no":
+        selfview.endView()
+        return False
+    else:
+        yes_or_no = selfview.invalid_input(
+            "Should the program run the solving algorithm?\n")
+        should_we_solve(selfview, yes_or_no)
+
 
 def set_grid(maze):
     global grid
     grid = maze
 
+
 def get_steps():
     global stepcount
     return stepcount
+
 
 def reset_steps():
     global stepcount
--- test_backend_functionality.py
+++ test_backend_functionality.py
@@ -2,8 +2,6 @@ import unittest
 import backend_functionality
 
 
-
-
 class TestBackend(unittest.TestCase):
 
     def test_increment(self):
@@ -12,8 +10,17 @@ class TestBackend(unittest.TestCase):
         self.assertEqual(stepcount, 0)
 
     def test_get_steps(self):
-        #Steps is a global variable in backend_functionality which starts by being -1
+        # Steps is a global variable in backend_functionality which starts by being -1
         self.assertEqual(backend_functionality.get_steps(), -1)
+
+    def test_reset_steps(self):
+        backend_functionality.reset_steps()
+
+        self.assertEqual(backend_functionality.get_steps(), -1)
+
+    def test_make_empty_maze(self):
+        self.assertEqual(backend_functionality.make_empty_maze(5, 5), [[[], [], [], [], []], [
+                         [], [], [], [], []], [[], [], [], [], []], [[], [], [], [], []], [[], [], [], [], []]])
 
 
 if __name__ == '__main__':
